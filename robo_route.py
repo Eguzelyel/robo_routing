a85c4ae35ffe1b0445760a4f6332cdf5a07fee1d
Luke Logan <lukemartinlogan@gmail.com>
2019-03-12 18:18:00 -0500

    Updated the data download methods

--- robo_route.py
+++ robo_route.py
@@ -2,6 +2,9 @@
 """
 This file predetermines the route a robot should take in order
 to fill as many potholes as possible.
+
+https://gps-coordinates.org/
+https://github.com/gboeing/osmnx-examples
 """
 
 import pandas as pd
@@ -10,22 +13,135 @@ import networkx as nx
 import osmnx as ox
 from CppGraph import CppGraph
 
-#Get the graph of Chicago
-def chicago_graph():
+def download_chicago_graph():
+	
+	"""
+	Download the graph of Chicago from Open Street Map and
+	put into a NetworkX MultiDiGraph.
+	
+	Return: 
+		A NetworkX MultiDiGraph.
+	"""
+
 	G = ox.graph_from_place("Chicago,IL, United States", network_type='drive')
 	return G
+	
 
-#Download pothole coordinates of only open potholes and map to OSM nodes
+def save_chicago_graph(G, path="chicago.xml"):
+
+	"""
+	Saves the graph of Chicago to a shape file, which
+	can handle DiGraph format.
+	
+	Inputs:
+		G: the graph of Chicago
+		path: the location to save this graph to
+	"""
+
+	ox.save_graphml(G, filename=path)
+
+
+def update_chicago_graph(path="chicago.xml"):
+
+	"""
+	Updates the data in the saved graph of Chicago.
+	
+	Inputs:
+		path: the location of the Chicago graph on your disk
+	"""
+
+	Chicago = download_chicago_graph()
+	save_chicago_graph(Chicago, path)
+	
+
+def open_chicago_graph(path="chicago.xml"):
+	
+	"""
+	Opens the saved graph of Chicago and returns a
+	NetworkX DiGraph of Chicago.
+	
+	Return:
+		A NetworkX DiGraph of Chicago
+	"""
+	
+	return ox.load_gdf_shapefile(G, filename=path)
+
+
 def download_potholes():
-	link = "https://data.cityofchicago.org/resource/_311-potholes.csv?$select=latitude,longitude,status"
+
+	"""
+	Download a CSV of potholes from the Chicago Data Portal.
+	The CSV will have two rows when it is returned:
+		1. Latitude
+		2. Longitude
+	These values are the coordinates of potholes in Chicago.
+	
+	Return:
+		A pandas data frame of locations of potholes in Chicago
+	"""
+
+	link = "https://data.cityofchicago.org/api/views/7as2-ds3y/rows.csv?accessType=DOWNLOAD"
 	df = pd.read_csv(link)
-	df = df[(df.status == "Open") | (df.status == "Open - Dup")]
-	df = df[["latitude", "longitude"]]
-	df = df.dropna(axis =0, subset=["longitude", "latitude"])
+	df = df[(df.STATUS == "Open") | (df.STATUS == "Open - Dup")]
+	df = df[["LATITUDE", "LONGITUDE"]]
+	df = df.dropna(axis =0, subset=["LATITUDE", "LONGITUDE"])
+	return df
+	
+
+def save_potholes(df, path="potholes.csv"):
+	
+	"""
+	This function saves the pothole data frame downloaded from
+	the Chicago Data Portal in a CSV file.
+	
+	Input:
+		df: The pothole data frame
+		path: The location to save the data frame to
+	"""
+	
+	df.to_csv(path)
+
+
+def update_potholes(path="potholes.csv"):
+
+	"""
+	This function updates the pothole data frame stored locally.
+	
+	Input:
+		path: The location of the pothole data stored locally
+	"""
+	
+	df = download_potholes()
+	save_potholes(df, path)
+	
+
+def open_potholes(path="potholes.csv"):
+
+	"""
+	This function opens the pothole data frame stored locally.
+	
+	Input:
+		path: The location of the pothole data stored locally
+	"""
+
+	df = pd.read_csv(path)
 	return df
 
-#Find shortest path between one pothole and the one its closest to
+
 def short_path1(Chicago, Cpp, src, dst, potholes):
+
+	"""
+	This function finds the shortest path between one pothole
+	and the pothole who is closest to it. It adds the path to
+	Cpp.
+	
+	Inputs:
+		Chicago: a MultiDiGraph of Chicago
+		Cpp: A CppGraph to add the shortest path to
+		src: The source pothole
+		dst: The pothole closest to the source pothole
+	"""
+
 
 	#Find the node closest to the central pothole
 	src_node = ox.get_nearest_node(Chicago, src)
@@ -44,12 +160,19 @@ def short_path1(Chicago, Cpp, src, dst, potholes):
 	Cpp.add_route(route)
 	
 
-
-#Find the list of edges to use in the robot's route
 def short_pathN(Chicago, Cpp, potholes):
 
-	#Get list of the (x, y)
-	coords = []
+	"""
+	This function finds the set of edges to be traversed in the
+	robot's route. It adds the edges to Cpp.
+	
+	Input:
+		Chicago: A MultiDiGraph of Chicago
+		Cpp: A CppGraph to add the edges to
+		potholes: A pandas dataframe of pothole data
+	"""
+
+	#
 	coords.append(Cpp.start)
 	for i in potholes:
 		coords.append()
@@ -62,24 +185,43 @@ def short_pathN(Chicago, Cpp, potholes):
 		dst = (float(row["latitude"]), float(row["longitude"]))
 		short_path1(Chicago, Cpp, src, dst, potholes)
 
-#Run the CPP over the edge list
-def get_robo_route(start=()):
 
-	print("Downloading Chicago")
-	#Chicago = chicago_graph()
-	#Cpp = CppGraph(Chicago)
-	#Cpp.set_start(start)
+def get_robo_route(start=(41.814884, -87.664603), chicago_path="chicago.xml", pothole_path="potholes.xml"):
+
+	"""
+	This function will compute the optimal route for
+	a robot to take to fill potholes.
 	
-	print("Downloading Potholes")
-	potholes = download_potholes()
+	Input:
+		start: The location where the robot starts and ends at (the facility)
+		chicago_path: The location of the graph of Chicago located locally
+		pothole_path: The location of the graph of the pothole data located locally
+	Return:
+		An ordered list of nodes representing the route the robot will take
+	"""
+
+	#Acquire the graph of Chicago
+	print("Opening Chicago")
+	Chicago = open_chicago_graph(chicago_path)
+	Cpp = CppGraph(Chicago)
+	Cpp.set_start(start)
 	
+	#Acquire the set of all potholes that need to be filled
+	print("Opening Potholes")
+	potholes = open_potholes(pothole_path)
+	
+	#Get the shortest paths connecting the potholes and the robot facility
 	print("Adding potholes to Chicago")
 	short_pathN(Chicago, Cpp, potholes)
 	
+	#Find the optimal way to traverse the graph starting from the robot's facility
 	print("Finding the route")
 	route = Cpp.solve()
 	
+	#Save the route the robot will take as an image
 	fig, ax = ox.plot_graph_route(G, route, save=True, filename="graph")
 	return route
 
-get_robo_route()
+update_chicago_graph("D:/Documents/School/S19/MATH 497/robo_routing/chicago.xml")
+#update_potholes("D:/Documents/School/S19/MATH 497/robo_routing/potholes.csv")
+#open_potholes("D:/Documents/School/S19/MATH 497/robo_routing/potholes.csv")
